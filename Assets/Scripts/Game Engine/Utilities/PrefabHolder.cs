24abc01e6868eba64716c4b8edf5c2da0f5601a1
WillBlackney <44806865+WillBlackney@users.noreply.github.com>
2020-09-09 13:16:23 +0200

    Asset + Prefab Cleanup

--- Assets/Scripts/Game Engine/Utilities/PrefabHolder.cs
+++ Assets/Scripts/Game Engine/Utilities/PrefabHolder.cs
@@ -8,35 +8,15 @@ public class PrefabHolder : Singleton<PrefabHolder>
     #region
     [Header("Buttons + UI")]
     public GameObject PassiveIconViewPrefab;
-    public GameObject AbilityButtonPrefab;
-    public GameObject abilityPageAbility;
-    public GameObject consumable;
-    public GameObject AttributeTab;
-    public GameObject enemyPanelAbilityTab;
     public GameObject activationWindowPrefab;
     public GameObject statePrefab;
-    public GameObject afflicationOnPanelPrefab;
 
-    [Header("Living Entity Prefabs")]
+    [Header("Character Entity Prefabs")]
     public GameObject characterEntityModel;
-    public GameObject defenderPrefab;
-    public GameObject enemyPrefab;
 
     [Header("Cards Prefabs")]
     public GameObject noTargetCard;
     public GameObject targetCard;
-
-    [Header("Loot Related")]
-    public GameObject GoldRewardButton;
-    public GameObject ConsumableRewardButton;
-    public GameObject ItemRewardButton;
-    public GameObject stateRewardButton;
-    public GameObject ArtifactGO;
-    public GameObject ItemCard;
-    public GameObject StateCard;
-    public GameObject InventoryItem;
-    public GameObject AbilityTomeInventoryCard;
-    public GameObject TreasureChest;
 
     [Header("Activation Window Related")]
     public GameObject panelSlotPrefab;
